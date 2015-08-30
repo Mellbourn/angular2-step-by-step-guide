07fabeb4610734d814f2f7f403589b5af689d11a
Klas Mellbourn <klas@mellbourn.net>
2015-08-30 13:21:55 +0200

    no new window, cause it is hard to keep track of otherwise

--- Start-Project.ps1
+++ Start-Project.ps1
@@ -1,3 +1,3 @@
 Remove-Item *.js
-Start-Process -NoNewWindow .\node_modules\.bin\tsc -ArgumentList '--watch', '-m', 'commonjs', '-t', 'es5', '--experimentalDecorators', '--emitDecoratorMetadata', 'app.ts' 
+Start-Process .\node_modules\.bin\tsc -ArgumentList '--watch', '-m', 'commonjs', '-t', 'es5', '--experimentalDecorators', '--emitDecoratorMetadata', 'app.ts', 'show-properties.ts' 
 .\node_modules\.bin\http-server
