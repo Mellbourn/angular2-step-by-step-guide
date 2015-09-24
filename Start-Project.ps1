aee6e40269026276e0c593616415ab037426b967
Klas Mellbourn <klas@mellbourn.net>
2015-09-24 20:00:00 +0200

    switching to ES6

--- Start-Project.ps1
+++ Start-Project.ps1
@@ -1,3 +1,3 @@
 Remove-Item *.js
-Start-Process .\node_modules\.bin\tsc -ArgumentList '--watch', '-m', 'commonjs', '-t', 'es5', '--experimentalDecorators', '--emitDecoratorMetadata', 'app.ts', 'display.component.ts', 'friends.service.ts', 'todo.list.ts' 
+Start-Process .\node_modules\.bin\tsc -ArgumentList '--watch', '-t', 'es6', '--experimentalDecorators', '--emitDecoratorMetadata', 'app.ts', 'display.component.ts', 'friends.service.ts', 'todo.list.ts' 
 .\node_modules\.bin\http-server
